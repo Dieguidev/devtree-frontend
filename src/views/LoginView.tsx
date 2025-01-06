8f0ef7ce41e65346ae0ec688f8fc43abbceee1e2
Dieguidev <diegogaraycullas@gmail.com>
2025-01-05 19:06:13 -0500

    Agregar AuthLayout y mejorar la estructura de navegación entre vistas de inicio de sesión y registro

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,12 +3,8 @@ import { Link } from "react-router-dom";
 export const LoginView = () => {
   return (
     <>
-      <div>LoginView</div>
-
       <nav>
-        <Link to="/auth/register">
-        ¿No tienes cuenta? Regístrate
-        </Link>
+        <Link to="/auth/register">¿No tienes cuenta? Regístrate</Link>
       </nav>
     </>
   );
