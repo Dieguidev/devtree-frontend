685644ece513279bc5943886eaeac13a498bb071
Dieguidev <diegogaraycullas@gmail.com>
2025-01-14 17:37:18 -0500

    Mostrando los mensajes desde el backend

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -2,75 +2,99 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { ErrorFormMessage } from "../components/ErrorFormMessage";
 import { LoginForm } from "../types";
+import { api } from "../config/axios";
+import { toast } from "sonner";
+import { isAxiosError } from "axios";
 
 export const LoginView = () => {
-
-  const initialValues: LoginForm ={
-    email: '',
-    password: ''
-  }
+  const initialValues: LoginForm = {
+    email: "",
+    password: "",
+  };
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({ defaultValues: initialValues });
 
-  const handleLogin = (formData: LoginForm) => {
-    console.log(formData);
+  const handleLogin = async (formData: LoginForm) => {
+    try {
+      await api.post(`/api/auth/login`, formData);
+      toast.success("Autenticado");
 
-  }
+      reset();
+    } catch (error) {
+      if (isAxiosError(error)) {
+        //para node
+        console.log(error.response?.data.error);
+        //para java
+        // console.log(error.response?.data);
+        if (
+          error.response?.data.error === "Invalid credentials" ||
+          error.response?.data === "Invalid credentials"
+        ) {
+          toast.error("Credenciales inválidas");
+        }
+      }
+    }
+  };
 
   return (
     <>
       <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
 
       <form
-    onSubmit={handleSubmit(handleLogin)}
-    className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
-    noValidate
->
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
-        <input
+        onSubmit={handleSubmit(handleLogin)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
             id="email"
             type="email"
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register("email", {
-                required: "El Email es obligatorio",
-                pattern: {
-                    value: /\S+@\S+\.\S+/,
-                    message: "E-mail no válido",
-                },
+              required: "El Email es obligatorio",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "E-mail no válido",
+              },
             })}
-        />
-        {errors.email && (
+          />
+          {errors.email && (
             <ErrorFormMessage>{errors.email.message}</ErrorFormMessage>
-        )}
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
-        <input
+          )}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
             id="password"
             type="password"
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register("password", {
-                required: "El Password es obligatorio",
+              required: "El Password es obligatorio",
             })}
-        />
-        {errors.password && (
+          />
+          {errors.password && (
             <ErrorFormMessage>{errors.password.message}</ErrorFormMessage>
-        )}
-    </div>
+          )}
+        </div>
 
-    <input
-        type="submit"
-        className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
-        value='Iniciar Sesión'
-    />
-</form>
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Iniciar Sesión"
+        />
+      </form>
 
       <nav>
         <Link
