06773a8609cdcda0b3d1193a72d763b2b1a6e99b
Dieguidev <diegogaraycullas@gmail.com>
2025-01-15 17:31:17 -0500

    Obteniendo la información del usuario y mostrándola en el componente

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,9 +1,10 @@
 import { isAxiosError } from 'axios';
 import { api } from '../config/axios';
+import { User } from '../types';
 
 export async function getUser() {
   try {
-    const { data } = await api.get('/api/user', );
+    const { data } = await api.get<User>('/api/user', );
     return data;
   } catch (error) {
     if (isAxiosError(error)) {
