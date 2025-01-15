4a7ce6f864822d59ffe191df6a0396c52a194284
Dieguidev <diegogaraycullas@gmail.com>
2025-01-14 20:29:42 -0500

    Añadiendo soporte para React Query y configurandolo con los devtools

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,11 @@ import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import NavigationTabs from "../components/NavigationTabs";
 
+
 export default function AppLayout() {
+
+
+
   return (
     <>
       <header className="bg-slate-800 py-5">
