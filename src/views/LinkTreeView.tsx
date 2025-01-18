77170b86cb2d38e8ca1169443b76810489f08df6
Dieguidev <diegogaraycullas@gmail.com>
2025-01-17 23:21:54 -0500

    Agregando el Array de Redes Sociales

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
+import { social } from "../data/social"
 
 
 export const LinkTreeView = () => {
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
   return (
     <div>LinkTreeView</div>
   )
