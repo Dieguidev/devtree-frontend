28fdcf823f9ddca0d2f849aeed183e02fed4793b
Dieguidev <diegogaraycullas@gmail.com>
2025-01-19 10:45:51 -0500

    Mostrando Handle, Imagen y Descripción

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -6,6 +6,10 @@ type HandleDataProps = {
 
 export const HandleData = ({data}: HandleDataProps) => {
   return (
-    <div>HandleData</div>
+    <div className="space-y-6 text-white">
+      <p className="text-5xl text-center font-black">{data.handle}</p>
+      {data.image && <img src={data.image} alt={data.handle} className="max-w-[250px] mx-auto" />}
+      <p className="text-lg text-center font-bold">{data.description}</p>
+    </div>
   )
 }
