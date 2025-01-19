a6b20af6c818dfcb3046cd044e0dc3f76cc8730d
Dieguidev <diegogaraycullas@gmail.com>
2025-01-19 10:03:41 -0500

    Consultando el Tree con React Query

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -14,8 +14,6 @@ export async function getUser() {
 }
 
 export async function updateProfile(formData: User) {
-  console.log(formData);
-
   try {
     const { data } = await api.put<string>('/api/user', formData);
     return data;
@@ -33,6 +31,19 @@ export async function uploadImage(file: File) {
   formData.append('file', file);
   try {
     const { data } = await api.post('/api/user/upload-image', formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.log(error.response?.data.error);
+
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function getUserByHandle(handle: string) {
+  try {
+    const { data } = await api.get<string>(`/api/user/${handle}`);
     return data;
   } catch (error) {
     if (isAxiosError(error)) {
--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,6 +1,21 @@
+import { useQuery } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { getUserByHandle } from "../api/DevTreeAPI"
 
 
 export const HandleView = () => {
+
+  const params = useParams()
+  const handle = params.handle!
+  const { data, error, isLoading} = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ['handle', handle],
+    retry: 1
+  })
+
+  console.log(data);
+
+
   return (
     <div>HandleView</div>
   )
