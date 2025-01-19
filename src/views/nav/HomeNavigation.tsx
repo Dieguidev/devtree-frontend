9bdbb1fa6afc5015dff02fea0d4f1d32dd679e0a
Dieguidev <diegogaraycullas@gmail.com>
2025-01-19 12:37:47 -0500

    Creando un Componente re-utilizable para el Logo y Navegaciones

--- src/views/nav/HomeNavigation.tsx
+++ src/views/nav/HomeNavigation.tsx
@@ -8,6 +8,11 @@ export const HomeNavigation = () => {
         className="text-white p-2 uppercase font-black text-xs cursor-pointer"
         to='/auth/login'
       >Iniciar Sesión</Link>
+
+      <Link
+        className="bg-lime-500 text-slate-800 p-2 uppercase font-black text-xs cursor-pointer rounded-lg"
+        to='/auth/register'
+      >Registrarme</Link>
     </>
   )
 }
